923f326a20c431000f119b134cc587d91ada7f4f
Plinio <mcplinio.15@gmail.com>
2024-03-16 02:02:27 -0500

    enpoind para crear mascota

--- src/main/java/com/proyectojava/controller/MascotaController.java
+++ src/main/java/com/proyectojava/controller/MascotaController.java
@@ -1,4 +1,23 @@
 package com.proyectojava.controller;
 
-public class MascotaController {
+import com.proyectojava.model.Mascota;
+import com.proyectojava.service.IMascotaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MascotaController{
+
+    @Autowired // inyección de la interfaz service para solicitar la info
+    private IMascotaService mascoServ;
+
+    // enpoint para crear o dar de alta una mascota
+    @PostMapping("/mascotas/crear")
+    public String saveMascota (@RequestBody Mascota masco) {
+        mascoServ.saveMascota(masco);
+
+        return "La mascota fue creada correctamente";
+    }
 }
