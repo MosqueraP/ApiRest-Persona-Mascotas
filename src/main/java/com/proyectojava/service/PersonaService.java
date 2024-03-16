1542cf3e51f9d4535842dbd1cb68e2fe8be81074
Plinio <mcplinio.15@gmail.com>
2024-03-15 21:47:40 -0500

    implementación de métodos

--- src/main/java/com/proyectojava/service/PersonaService.java
+++ src/main/java/com/proyectojava/service/PersonaService.java
@@ -1,12 +1,35 @@
 package com.proyectojava.service;
 
+import com.proyectojava.model.Persona;
 import com.proyectojava.repository.IPersonaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class PersonaService{
+public class PersonaService implements IPersonaService{
 
     @Autowired
     private IPersonaRepository personaRepo;
+
+    @Override
+    public List<Persona> getPersonas() {
+        return null;
+    }
+
+    @Override
+    public void savePersona(Persona persona) {
+
+    }
+
+    @Override
+    public void deletePersona(Long id) {
+
+    }
+
+    @Override
+    public Persona findPersona(Long id) {
+        return null;
+    }
 }
