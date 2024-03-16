80d2512509e4db1926d7def1d8772e181aa1d705
Plinio <mcplinio.15@gmail.com>
2024-03-15 21:45:46 -0500

    Crear metodos abstractos

--- src/main/java/com/proyectojava/service/IPersonaService.java
+++ src/main/java/com/proyectojava/service/IPersonaService.java
@@ -1,7 +1,24 @@
 package com.proyectojava.service;
 
 import com.proyectojava.model.Persona;
-import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface IPersonaService extends JpaRepository<Persona, Long> {
+import java.util.List;
+
+public interface IPersonaService{
+    // métodos abstractos
+
+    // método traer todas las personas (LECTURA)
+    public List<Persona> getPersonas();
+
+    // método guardar una persona (ALTA)
+    public void savePersona(Persona persona);
+
+    // método guardar una persona (BAJA)
+    public void deletePersona(Long id);
+
+    // método buscar un solo objeto (LECTURA )
+    public Persona findPersona(Long id);
+
+    // método eliminar una persona ()
+
 }
